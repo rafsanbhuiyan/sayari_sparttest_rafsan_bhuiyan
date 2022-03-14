d1a4c44fa26922b2f9265c3bb38a7827ba547937
Rafsan Bhuiyan <48545950+rafsanbhuiyan@users.noreply.github.com>
2022-03-13 21:52:27 -1000

    Update spark_test.py

--- spark_test.py
+++ spark_test.py
@@ -1,15 +1,7 @@
-from sqlite3 import DateFromTicks
-import pyspark
-import pandas as pd
-import json
 from pyspark.sql import SparkSession
-from pyspark import SparkConf, SparkContext
+from pyspark import SparkContext
 from pyspark.sql import functions as sf
 from pyspark.sql.functions import *
-
-
-
-
 
 sc = SparkContext(master="local[*]", appName = "data_json")
 
@@ -19,32 +11,25 @@ normal_gbr_df = spark.read.json("/Users/rafsanbhuiyan/Documents/GitHub/sayari_sp
 
 normal_ofac_df = spark.read.json("/Users/rafsanbhuiyan/Documents/GitHub/sayari_sparttest_rafsan_bhuiyan/ofac.jsonl")
 
-#normal_gbr_df.show()
 
-
-
-#normal_gbr_df.printSchema()
-
-
-
-gz = normal_gbr_df.withColumn("country",normal_gbr_df.addresses.country)
-gz = gz.withColumn("postal_code",normal_gbr_df.addresses.postal_code)
+gbr = normal_gbr_df.withColumn("country",normal_gbr_df.addresses.country)
+gbr = gbr.withColumn("postal_code",normal_gbr_df.addresses.postal_code)
 #gz = gz.withColumn("reported_DOB", concat_ws(", ", normal_gbr_df.reported_dates_of_birth))
 
 #explode
-explode_gz = gz.withColumn("reported_DOB", explode("reported_dates_of_birth"))
+explode_gbr = gbr.withColumn("reported_DOB", explode("reported_dates_of_birth"))
 
 
-explode_gz = explode_gz.withColumn("id_number", explode("id_numbers")).withColumn("nationality", explode("nationality"))
+explode_gbr = explode_gbr.withColumn("id_number", explode("id_numbers")).withColumn("nationality", explode("nationality"))
 
-explode_gz = explode_gz.withColumn("comment", explode_gz.id_number.comment).withColumn("value",explode_gz.id_number.value).withColumn("country", explode("country")).withColumn("postal_code", explode("postal_code"))
+explode_gbr = explode_gbr.withColumn("comment", explode_gbr.id_number.comment).withColumn("value",explode_gbr.id_number.value).withColumn("country", explode("country")).withColumn("postal_code", explode("postal_code"))
 
 
-gbr_df = explode_gz.select("id","comment","value", "name", "nationality", "place_of_birth", "position", "type", to_date("reported_DOB", 'dd/MM/yyyy').alias("reported_DOB"), "country", "postal_code")
+gbr_df = explode_gbr.select("id","comment","value", "name", "nationality", "place_of_birth", "position", "type", to_date("reported_DOB", 'dd/MM/yyyy').alias("reported_DOB"), "country", "postal_code")
 
 gbr_df.printSchema()
 
-gbr_df.show(truncate = 50)
+gbr_df.show(truncate = 60)
 
 #gz_join = gz.join(explode_gz, on = "id", how = "inner")
 
